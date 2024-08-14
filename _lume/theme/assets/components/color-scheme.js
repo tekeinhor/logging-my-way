a644729e3ac1b7b5b62e8971066394f0743d664b
Tatiana <tatiana.ekeinhor@gmail.com>
2024-08-14 22:53:35 +0200

    refactor: reorganise theme folder

---
+++ _lume/theme/assets/components/color-scheme.js
@@ -0,0 +1,10 @@
+const toggle_switch = document.querySelector(".switch");
+const html = document.querySelector("html")
+
+if (toggle_switch){
+    toggle_switch.addEventListener("click", ()=> {
+        toggle_switch.classList.toggle("active")
+        html.classList.toggle("dark-mode")
+         
+    })
+}
