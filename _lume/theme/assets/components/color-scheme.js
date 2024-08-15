b48a1dd0c7d280986baa0b92eb990d5384631c94
Tatiana <tatiana.ekeinhor@gmail.com>
2024-08-15 13:49:29 +0200

    refactor(style): reorganise css values for theme

--- _lume/theme/assets/components/color-scheme.js
+++ _lume/theme/assets/components/color-scheme.js
@@ -5,6 +5,6 @@ if (toggle_switch){
     toggle_switch.addEventListener("click", ()=> {
         toggle_switch.classList.toggle("active")
         html.classList.toggle("dark-mode")
-         
+        globalThis.dispatchEvent(new CustomEvent("onChangeColorScheme"))
     })
 }
