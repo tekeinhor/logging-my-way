450a5cdbcdf9bef51d0c5b0698654130d2a2eaf3
Tatiana <tatiana.ekeinhor@gmail.com>
2024-08-19 10:18:15 +0200

    fix(theme): fix color gap on iOS device with meta "theme-color" and remove dead code in css

--- _lume/theme/assets/components/color-scheme.js
+++ _lume/theme/assets/components/color-scheme.js
@@ -12,9 +12,11 @@ if (toggle_switch){
         if (is_dark_mode){
             localStorage.setItem("color-scheme", "dark")
             document.documentElement.setAttribute("data-color", "dark");
+            document.querySelector("meta[name='theme-color']").setAttribute("content", "#1c1e20");
         }else{
             localStorage.setItem("color-scheme", "light")
             document.documentElement.setAttribute("data-color", "light");
+            document.querySelector("meta[name='theme-color']").setAttribute("content", "#eaeaea");
         }
     })
 }
